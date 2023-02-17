8b6cdba0f90b9a192463218efb54e9b842438721
Khanh Linh Tran <KhanhLinh.Tran01@student.csulb.edu>
2023-02-16 19:50:35 -0800

    1st Release

--- js/ex5.js
+++ js/ex5.js
@@ -1,4 +1,13 @@
 /* Homework 4
    Exercise 5 JavaScript code
-   -- 
+   -- List of words
 */
+const word = [];
+add = prompt("Please enter a word, enter 'Stop' to stop:");
+while (add.toLowerCase() !== "stop") {
+   word.push(add);
+   add = prompt("Please enter a word, enter 'Stop' to stop:");
+}
+for (let i = 0; i < word.length; i++) {
+   console.log(word[i]);
+}
--- js/ex6.js
+++ js/ex6.js
@@ -1,3 +1,20 @@
 /* Homework 4
    Exercise 6 JavaScript code
 */
+word = prompt("Please enter a word:");
+count = 0;
+for (let i = 0; i < word.length; i++) {
+   if ('aeiou'.includes(word[i].toLowerCase())) {
+     count += 1;
+   }
+}
+function checkPalindrome(word) {    
+   var l = word.length;
+   for (var i = 0; i < l / 2; i++) {
+       if (word.charAt(i) !== word.charAt(l - 1 - i)) {
+           return "not a palindrome";
+       }
+   }
+   return "a palindrome";
+}
+console.log(`${word} contains ${count} vowel(s) and is ${checkPalindrome(word)}`);
